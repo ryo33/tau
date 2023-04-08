3ae0e7ddf7fc0f922400eafc6698e3b9cc0fc35c
Ryo Hirayama <ryo12redstone@gmail.com>
2023-04-08 23:14:48 +0900

    Add tau.pl

---
+++ prolog/tau.pl
@@ -0,0 +1,13 @@
+:- op(500, xfx, [:, !]). % infix operators
+:- op(600, xfx, [→]). % effect
+:- op(700, xfx, [:⇐, :⇒, <:, ≦:, ⇓]).
+:- op(800, xfx, ⊢).
+:- op(900, xfx, ⊣).
+
+% Evaluation
+Γ ⊢ ref(t, []) ⊣ Γ ⇓ v :- member((t, v), Γ).
+Γ1 ⊢ ref(product(x, f), [a | tail]) ⊣ Γ2 ⇓ v :- a <: x, [(x, a) | Γ1] ⊢ ref(f, tail) ⇓ v ⊣ Γ2.
+% Γ1 ⊢ ref(sum(x, f), [a | tail]) ⇓ v ⊣ Γ3 :- TODO.
+
+% Subtyping
+a <: a.
